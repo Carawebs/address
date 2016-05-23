3080990285b1d4b59d201fa5fb171b48001c52c2
David Egan <david@carawebs.com>
2016-05-23 21:33:32 +0100

    Remove unnecessary methods

--- admin/class-address-admin.php
+++ admin/class-address-admin.php
@@ -68,52 +68,6 @@ class Address_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Address_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Address_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/address-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Address_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Address_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/address-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Add an options page under the Settings submenu
 	 *
 	 * @see http://ottopress.com/2009/wordpress-settings-api-tutorial/
